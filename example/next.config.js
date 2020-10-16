230733b01167f4f6cd6826d202f29ee6246b4792
yanyanlongxia <59304287+yanyanlongxia@users.noreply.github.com>
2020-10-16 17:01:00 +0800

    Update

--- example/next.config.js
+++ example/next.config.js
@@ -7,7 +7,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   async redirects() {
     return [
-      // redirect the index page to our notion test suite
       {
         source: '/',
         destination: '/79b255e9f2074c9691e88e40dc79f054',
